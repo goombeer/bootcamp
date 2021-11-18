f0a7fa2fb68b61761aea3983461cc2f576484544
goombeer <highbridge71@gmail.com>
2021-11-18 20:15:45 +0900

    first step

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserEditComponent } from './user/user-edit/user-edit.component';
+import { UserService } from './user/user.service';
+
+import { FormsModule } from '@angular/forms';
+import { ConfirmDialogComponent } from './tools/confirm-dialog/confirm-dialog.component';
+
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    DashboardComponent,
+    UserListComponent,
+    UserEditComponent,
+    ConfirmDialogComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    MatDialogModule,
+    BrowserAnimationsModule
+  ],
+  providers: [
+    UserService
+  ],
+  entryComponents: [
+    ConfirmDialogComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
